c9f57e36c88e1121712a831005984ff2fe30aa1b
KaishaerMahemuti <k.mahemuti@gmail.com>
2025-03-26 12:59:10 -0400

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="container mt-5">
+      <h1 className="text-center"> Test Genie</h1>
+      <p className="text-center">Paste code or upload a file to generate tests</p>
+    </div>
+  );
+};
+
+export default App;
